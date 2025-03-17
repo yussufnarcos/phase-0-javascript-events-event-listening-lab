dee923479f2ca311fabcaaf43c981696038ee14e
yussufnarcos <narcosyussuf@gmail.com>
2025-03-17 10:48:06 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.getElementById("button");
+    if (button) {
+        button.addEventListener("click", function() {
+            console.log("Button clicked!");
+        });
+    }
 }
